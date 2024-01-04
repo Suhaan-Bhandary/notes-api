fa2e89d2126d9ec10d6ed47c5fc4ac2769b92308
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-01-04 15:37:12 +0530

    feat: Created auth service tests

--- src/tests/services/auth.services.test.ts
+++ src/tests/services/auth.services.test.ts
@@ -1,6 +1,78 @@
-describe('env', () => {
-  test('hi', () => {
-    console.log(process.env['DATABASE']);
-    expect(1 + 2).toBe(3);
+import bcrypt from 'bcryptjs';
+import { db } from '../../db/db';
+import { authService } from '../../services';
+
+describe('Auth Service', () => {
+  afterEach(async () => {
+    await db.deleteFrom('user').execute();
+  });
+
+  // Test
+  describe('createUser', () => {
+    test('New and Valid User', async () => {
+      let user = {
+        email: 'test@gmail.com',
+        name: 'test',
+        password: 'test',
+      };
+
+      const hashPassword = await bcrypt.hash(user.password, 12);
+      user = {
+        ...user,
+        password: hashPassword,
+      };
+
+      await authService.createUser(user);
+
+      // Check if user is inserted in db
+      expect(async () => {
+        await db
+          .selectFrom('user')
+          .where('email', '=', user.email)
+          .executeTakeFirstOrThrow();
+      }).resolves;
+    });
+
+    test('Duplicate user Email', async () => {
+      let user = {
+        email: 'test@gmail.com',
+        name: 'test',
+        password: 'test',
+      };
+
+      const hashPassword = await bcrypt.hash(user.password, 12);
+      user = {
+        ...user,
+        password: hashPassword,
+      };
+
+      await authService.createUser(user);
+
+      // Check if user is inserted in db
+      expect(async () => {
+        await authService.createUser(user);
+      }).rejects.toThrow();
+    });
+  });
+
+  describe('getUserPassword', () => {
+    test('check the password matches with the original', async () => {
+      let user = {
+        email: 'test@gmail.com',
+        name: 'test',
+        password: 'test',
+      };
+
+      const hashPassword = await bcrypt.hash(user.password, 12);
+      user = {
+        ...user,
+        password: hashPassword,
+      };
+
+      await authService.createUser(user);
+      const password = await authService.getUserPassword(user.email);
+
+      expect(password).toBe(user.password);
+    });
   });
 });
