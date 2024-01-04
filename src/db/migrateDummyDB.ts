8bafba0cec21487650c2c6e91a21b52e395a2486
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-01-04 15:06:00 +0530

    feat: added script to migrate db to latest

---
+++ src/db/migrateDummyDB.ts
@@ -0,0 +1,39 @@
+// Initializing the env file
+import { config } from 'dotenv';
+import path from 'path';
+config({ path: path.join(__dirname, '../../.test.env') });
+
+import { promises as fs } from 'fs';
+import { FileMigrationProvider, Migrator } from 'kysely';
+import { db } from './db';
+
+async function migrateToLatest() {
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      migrationFolder: path.join(__dirname, './migrations'),
+    }),
+  });
+
+  const { error, results } = await migrator.migrateToLatest();
+
+  results?.forEach((it) => {
+    if (it.status === 'Success') {
+      console.log(`migration "${it.migrationName}" was executed successfully`);
+    } else if (it.status === 'Error') {
+      console.error(`failed to execute migration "${it.migrationName}"`);
+    }
+  });
+
+  if (error) {
+    console.error('failed to migrate');
+    console.error(error);
+    process.exit(1);
+  }
+
+  await db.destroy();
+}
+
+migrateToLatest();
