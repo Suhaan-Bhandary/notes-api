80876b94ae6bbc2e7cf05682f501434de06b6c60
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2024-01-04 11:41:48 +0530

    fix: limit typo in comment

--- src/config/rate-limiter.ts
+++ src/config/rate-limiter.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const nonAuthLimiter = rateLimit({
   windowMs: (process.env['WINDOW_SIZE_NON_AUTH_MIN'] || 1) * 60 * 1000, // 1 minutes
-  limit: process.env['LIMIT_NON_AUTH'] || 10, // Limit each IP to 20 requests per `window` (here, per 1 minutes).
+  limit: process.env['LIMIT_NON_AUTH'] || 10, // Limit each IP to 10 requests per `window` (here, per 1 minutes).
   message: 'Rate Limit Exceeded, Please try in 1min',
 });
 
